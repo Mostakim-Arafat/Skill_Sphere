79e2abc21c479f459680013edb8e72dc0f92e20e
Mostakim <mostakim2005arafat@gmail.com>
2026-05-15 12:12:45 +0600

    add search button

--- src/Components/Course_card.jsx
+++ src/Components/Course_card.jsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Course_card = ({ data }) => {
-    console.log(data)
     const {
         id,
         title,
--- src/app/Page/Course/page.jsx
+++ src/app/Page/Course/page.jsx
@@ -1,15 +1,29 @@
+'use client'
 import Data from '../../../Data/cousrse.json'
 import Course_card from '@/Components/Course_card';
-const page = () => {
-    
+import { useState } from 'react';
+const Course = () => {
+    const [search,setSearch] = useState('')
+    const handletext = (e) => {
+      setSearch(e.target.value)
+    }
+    const filtered = Data.filter( i => i.title.toLowerCase().includes(search))
+
     return (
         <div>
-            cousrse:
+            <div className='flex justify-around'>
+                <div>Courses</div>
+                <div>
+                   <input value={search} type="text" className='border border-amber-300' onChange={handletext} />
+                </div>
+            </div>
             <div className='grid grid-cols-3'>
-                { Data.map( data => <Course_card data={data} key={data.id}></Course_card>)}
+                { filtered.length>0 ?
+                filtered.map(data => <Course_card data={data} key={data.id}></Course_card>):
+                Data.map(data => <Course_card data={data} key={data.id}></Course_card>)}
             </div>
         </div>
     );
 };
 
-export default page;
+export default Course;
